c5de4ca573e9d775d83517ff4eab91f139b6eeef
Vassilis Bousbourelis <vasbousdev@gmail.com>
2025-02-20 15:34:04 +0100

    Solved lab

--- index.js
+++ index.js
@@ -6,9 +6,11 @@
 const string1 = "My favorite dessert is jello";
 
 // Your code here...
-
-
-
+for (i = 0; i < string1.length; i++) {
+  if (string1[i] === "j") {
+    console.log(i);
+  }
+}
 
 /*******************************************
     Iteration 2 | Concatenate Characters
@@ -18,9 +20,26 @@ const string1 = "My favorite dessert is jello";
 const string2 = "ABCDEFGHJKLO";
 
 // Your code here...
+let charC;
+let charO;
+let charL;
 
+function writeCool(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "C") {
+      charC = str[i];
+    } else if (str[i] === "O") {
+      charO = str[i];
+    } else if (str[i] === "L") {
+      charL = str[i];
+    }
+  }
+  let result = charC + charO + charO + charL;
+  console.log(result);
+  return result;
+}
 
-
+writeCool(string2);
 
 /*****************************************************
     Iteration 3 | Repeat a String and Concatenate
@@ -30,21 +49,17 @@ const string2 = "ABCDEFGHJKLO";
 const string3 = "Na";
 
 // Your code here...
-
-
-
-
+console.log(`${string3.repeat(4)} Batman!`);
 /*******************************************
        Iteration 4 | Fruite Slice
 *******************************************/
 // Using the string method .slice(), access and print to the console the name of your favorite fruit from a given string
 
-const fruit = "banana apple mango orange lemon kiwi watermelon grapes pear pineapple";
+const fruit =
+  "banana apple mango orange lemon kiwi watermelon grapes pear pineapple";
 
 // Your code here...
-
-
-
+console.log(fruit.slice(13, 18));
 /***************************************************
     Iteration 5 | Check If Strings Include a Word
 ***************************************************/
@@ -55,16 +70,20 @@ const fruit = "banana apple mango orange lemon kiwi watermelon grapes pear pinea
 const funnyHeadline1 = "Breathing oxygen linked to staying alive";
 const funnyHeadline2 = "Students Cook & Serve Grandparents";
 
-
 // Check the first headline
 // Your code here ...
-
-
+if (funnyHeadline1.includes("oxygen")) {
+  console.log("The string includes the word 'oxygen'");
+} else {
+  console.log("The string does not include the word 'oxygen'");
+}
 // Check the second headline
 // Your code here ...
-
-
-
+if (funnyHeadline2.includes("oxygen")) {
+  console.log("The string includes the word 'oxygen'");
+} else {
+  console.log("The string does not include the word 'oxygen'");
+}
 /*******************************************
        Iteration 6 | String Length
 *******************************************/
@@ -72,10 +91,9 @@ const funnyHeadline2 = "Students Cook & Serve Grandparents";
 
 const string4 = "zEAWrTC9EgtxmK9w1";
 
-
 // a) Print the string length
 // Your code here ...
-
-
+console.log(string4.length);
 // b) Print the last character in the string
 // Your code here ...
+console.log(string4[string4.length - 1]);
